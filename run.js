0162099cefdfe9e2bf5d880a801d5b50bbcc6925
Johannes Sorg <johannes@sorg.me>
2018-10-22 10:06:34 +0200

    improves naming and code structur

--- run.js
+++ run.js
@@ -4,7 +4,13 @@ const program = require('commander')
 const { spawn } = require('child_process')
 const inquirer = require('inquirer')
 
-const getDevices = (acc, curr) => {
+const exitWithErrorMessage = message => {
+  console.log('')
+  console.log(message)
+  process.exit(1)
+}
+
+const getAvailableOsWithDevices = (acc, curr) => {
   if (acc.currentDeviceSection && !curr.startsWith('--')) {
     const device = curr.match(/(.*)\s\(([a-zA-Z0-9-]+)\)\s\([a-zA-Z]+\)$/i)
     const isFirstArrayConstruction = acc.devices[acc.currentDeviceSection]
@@ -25,72 +31,89 @@ const getDevices = (acc, curr) => {
       : acc
   }
 
-  const match = curr.match(/^--\siOS\s([0-9.]+)\s--/)
-  return match
-    ? { ...acc, currentDeviceSection: match[1] }
+  const matchOsHeadline = curr.match(/^--\siOS\s([0-9.]+)\s--/)
+
+  return matchOsHeadline
+    ? { ...acc, currentDeviceSection: matchOsHeadline[1] }
     : { ...acc, currentDeviceSection: null }
 }
 
-const getEmulatorData = data =>
+const getAvailableEmulatorsData = data =>
   new Promise(resolve => {
     const outputArray = data
       .toString()
       .split('\n')
       .map(item => item.replace(/^ +(?=)/g, ''))
 
-    const filteredOutput = outputArray.reduce(getDevices, { devices: {} })
+    const filteredOutput = outputArray.reduce(getAvailableOsWithDevices, {
+      devices: {},
+    })
 
     resolve(filteredOutput.devices)
   })
+
+const chooseOsPrompt = ({ choices }) =>
+  choices.length > 1
+    ? inquirer.prompt([
+        {
+          type: 'list',
+          name: 'chosenOs',
+          message: 'Choose the OS you want to run:',
+          choices,
+          pageSize: 40,
+        },
+      ])
+    : Promise.resolve({ chosenOs: choices[0] })
+
+const chooseDevice = ({ choices }) =>
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'chosenDevice',
+      message: 'Choose the device you want to use:',
+      choices,
+      pageSize: 40,
+    },
+  ])
 
 const run = () => {
   const cmd = spawn('xcrun', ['simctl', 'list'])
 
   cmd.stdout.on('data', data =>
-    getEmulatorData(data)
+    getAvailableEmulatorsData(data)
       .then(devices => {
-        if (Object.keys(devices).length) {
-          inquirer
-            .prompt([
-              {
-                type: 'list',
-                name: 'chosenOS',
-                message: 'Choose the OS you want to run:',
-                choices: Object.keys(devices).reverse(),
-                pageSize: 40,
-              },
-            ])
-            .then(({ chosenOS }) =>
-              inquirer
-                .prompt([
-                  {
-                    type: 'list',
-                    name: 'chosenDevice',
-                    message: 'Choose the device you want to use:',
-                    choices: devices[chosenOS].map(item => item.name),
-                    pageSize: 40,
-                  },
-                ])
-                .then(({ chosenDevice }) => {
-                  const device = devices[chosenOS].reduce(
-                    (acc, curr) => (curr.name === chosenDevice ? curr : acc),
-                    {}
-                  )
+        const availableOs = Object.keys(devices)
 
-                  spawn('xcrun', ['simctl', 'boot', device.id])
-                  spawn('open', [
-                    '/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app',
-                  ])
-                })
-            )
+        if (availableOs.length) {
+          chooseOsPrompt({
+            choices: availableOs.reverse(),
+          }).then(({ chosenOs }) =>
+            chooseDevice({
+              choices: devices[chosenOs].map(item => item.name),
+            }).then(({ chosenDevice }) => {
+              const device = devices[chosenOs].reduce(
+                (acc, curr) => (curr.name === chosenDevice ? curr : acc),
+                {}
+              )
+
+              if (!device.id) {
+                exitWithErrorMessage("Couldn't find a matching emulator")
+              }
+
+              spawn('xcrun', ['simctl', 'boot', device.id])
+              spawn('open', [
+                '/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app',
+              ])
+            })
+          )
         }
       })
-      .catch(e => (console.error(e), process.exit(1)))
+      .catch(e => exitWithErrorMessage(e))
   )
 }
 
 program
   .version('0.1.0')
-  .usage('./launchEmulator.js')
+  .usage('launch-emulator')
   .action(run)
   .parse(process.argv)
